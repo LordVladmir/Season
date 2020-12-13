8659bcd5249e7dce6b85b3f0141aaae3a6f81b8d
MasterSaad <sdmamodesaeb@student.udm.ac.mu>
2020-12-13 23:35:41 +0400

    learn: state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  //get location
-  window.navigator.geolocation.getCurrentPosition((position) => 
-    console.log(position), (err) => console.log(err)
-  );
-  return <div>Hi there</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+
+    //get location
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if(this.state.errorMessage &&  !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if(!this.state.errorMessage && this.state.lat){
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
