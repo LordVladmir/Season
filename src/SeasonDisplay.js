b7f54d6fe6e4524bcd61ba12318a26259b6104e8
MasterSaad <sdmamodesaeb@student.udm.ac.mu>
2020-12-14 11:50:32 +0400

    learing of state 2 method -display season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        
+        return lat > 0 ? 'Summer1' : 'Winter';
+
+    }
+    else{
+        return lat < 0 ? 'Winter' : 'Summer2';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    console.log(new Date().getMonth());
+    console.log(season);
     return <div>Season Display</div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: "" };
-
+  state = { lat: null, errorMessage: "" };
+  componentDidMount() {
     //get location
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => {console.log(position.coords.latitude);
+        this.setState({ lat: position.coords.latitude })},
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    if(this.state.errorMessage &&  !this.state.lat){
-      return <div>Error: {this.state.errorMessage}</div>
-    }
+    // if (this.state.errorMessage && !this.state.lat) {
+    //   return <div>Error: {this.state.errorMessage}</div>;
+    // }
 
-    if(!this.state.errorMessage && this.state.lat){
-      return <div>Latitude: {this.state.lat}</div>
-    }
+    // if (!this.state.errorMessage && this.state.lat) {
+    //   return <div>Latitude: {this.state.lat}</div>;
+    // }
 
-    return <div>Loading!</div>
+    return (
+      <div>
+        <SeasonDisplay />
+      </div>
+    );
   }
 }
 
